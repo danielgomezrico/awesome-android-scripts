c50c53b3004c2dde8560e9346de0cb6dd2450ac5
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-01-26 11:26:41 -0500

    Fix android dependencies fail on bash

--- install-android-dependencies.sh
+++ install-android-dependencies.sh
@@ -4,17 +4,12 @@
 # May be used for ci or other team members.
 #
 
-dependencies=(
-      android-25
-      build-tools-25.0.2
-      tool
-      extra-android-m2repository
-      extra-android-support
-      extra-google-google_play_services
-      extra-google-m2repository
-)
+for I in android-25 \
+         build-tools-25.0.2  \
+         tool \
+         extra-android-m2repository \
+         extra-android-support \
+         extra-google-google_play_services \
+         extra-google-m2repository;
 
-for dependency in "${dependencies[@]}"
-do
-  echo y | android update sdk --no-ui --all --filter $dependency
-done
+ do echo y | android update sdk --no-ui --all --filter $I ; done
