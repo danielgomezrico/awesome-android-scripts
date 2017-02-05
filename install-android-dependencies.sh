b65f0825789d162f1698c4532b70cd43b0467c9b
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-02-05 19:41:08 -0200

    Add deps file to install deps

--- install-android-dependencies.sh
+++ install-android-dependencies.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 #
-# Install JUST the required dependencies for the project.
-# May be used for ci or other team members.
+# Install required dependencies
+# $ANDROID_HOME must be set to be cached on CI setup
 #
 
-for I in android-25 \
-         build-tools-25.0.2  \
-         tool \
-         extra-android-m2repository \
-         extra-android-support \
-         extra-google-google_play_services \
-         extra-google-m2repository;
+DEPS_FILE="$ANDROID_HOME/android-dependencies-installed"
 
- do echo y | android update sdk --no-ui --all --filter $I ; done
+if [ ! -e $DEPS_FILE ]; then
+
+  for I in android-25 \
+           build-tools-25.0.2  \
+           tool \
+           extra-android-m2repository \
+           extra-android-support \
+           extra-google-google_play_services \
+           extra-google-m2repository;
+   do echo y | android update sdk --no-ui --all --filter $I ; done
+
+   touch DEPS_FILE;
+
+ fi
