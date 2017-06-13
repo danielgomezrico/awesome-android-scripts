f78fb4f88d59dc1abc976a3c66b97815a632214f
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-06-13 09:10:35 -0500

    Update install script to use sdkmanager
    android command is deprecated

--- install-android-dependencies.sh
+++ install-android-dependencies.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 #
 # Install required dependencies
-# $ANDROID_HOME must be set to be cached on CI setup
+# sdkmanager can be found in $ANDROID_HOME/tools/bin/sdkmanager
 #
 
-DEPS_FILE="$ANDROID_HOME/android-dependencies-installed"
+# Accept licences
+# src http://vgaidarji.me/blog/2017/05/31/automatically-accept-android-sdkmanager-licenses/
+/usr/bin/expect -c '
+set timeout -1;
+spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager --licenses;
+  expect {
+    "y/N" { exp_send "y\r" ; exp_continue }
+    eof
+  }
+'
 
-if [ ! -e $DEPS_FILE ]; then
+for I in "platforms;android-25" \
+         "platforms;android-23" \
+         "platforms;android-21" \
+         "buildDate-tools;25.0.6" \
+         "extras;google;m2repository" \
+         "extras;android;m2repository" \
+         "extras;google;google_play_services"; do
+    echo "Trying to update with tools/bin/sdkmanager: " $I
+    sdkmanager $I
+done
 
-  for I in android-25 \
-           build-tools-25.0.2  \
-           tool \
-           extra-android-m2repository \
-           extra-android-support \
-           extra-google-google_play_services \
-           extra-google-m2repository;
-   do echo y | android update sdk --no-ui --all --filter $I ; done
-
-   touch DEPS_FILE;
-
- fi
+sdkmanager --update
