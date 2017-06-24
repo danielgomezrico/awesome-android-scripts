b6d2b969cd22e8843293befb83faa03e46f6e459
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-06-24 09:32:39 -0500

    Add comment to avoid this script in some cases

--- cp-env-to-properties.sh
+++ cp-env-to-properties.sh
@@ -2,9 +2,10 @@
 #
 # Copy ALL env variables to gradle properties file
 # 
-# Useful to setup easy any CI service.
-# 
 # You may want another output file then use PROPERTIES_FILE_PATH variable for that
+# 
+# Note: you can use System.getenv("VAR_NAME") instead of this if you dont need to create a local .properties files
+# in your project.
 #
 
 PROPERTIES_FILE_PATH=gradle.properties
