e62435b19054a2fa00cd71d53412ee844c78a2ca
Daniel Gomez <danielgomezrico@gmail.com>
2018-11-19 13:43:00 -0500

    Update awake screen

--- wait-for-device.sh
+++ wait-for-device.sh
@@ -11,7 +11,30 @@ adb_all () {
     done
 }
 
-adb_all shell input keyevent 26 # KEYCODE_POWER
+awake_screen() {
+  ANDROID_SDK_VERSION="$(adb shell getprop ro.build.version.sdk)"
+
+  if [ "$ANDROID_SDK_VERSION" -gt 19 ]; then
+    echo "Awaking android device."
+    adb_all shell input keyevent 224 # KEYCODE_WAKEUP just for < Android 19
+  else
+    echo "Checking if its needed to turn on the screen."
+
+    adb shell dumpsys power | grep -i 'Display Power: state=OFF'
+    if [ $? -eq 0 ]; then
+      echo "Screen is off. Wake up the phone"
+      adb shell input keyevent 26 # KEYCODE_POWER will turn of if it is turned on
+    else
+      echo "Screen is on so no action required."
+    fi
+  fi
+}
+
+awake_screen
 adb_all shell input keyevent 3 # HOME
+adb_all shell input keyevent 4 # BACK
+adb_all shell input keyevent 4 # BACK
+adb_all shell input keyevent 4 # BACK
+
 adb_all wait-for-device
-adb_all shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
+adb_all shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
